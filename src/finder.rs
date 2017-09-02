f4ebf26b0aae0be0557ceb5b490da05860e42398
Christos Klaoudianis <klaoudiani@ceid.upatras.gr>
2017-09-02 16:18:49 +0300

    Move parse_words fn to finder namespace

--- src/finder.rs
+++ src/finder.rs
@@ -1,4 +1,24 @@
-pub fn first_word(s: &[u8]) -> &[u8] {
+use std::str;
+
+pub fn parse_words (s: &String) -> Vec<&str> {
+
+    let mut words: Vec<&str> = Vec::new();
+
+    //i will be a cursor that will hold the index to the first character of the next word
+    let mut i = 0;
+    let size = s.len();
+    let raw_bytes = s.as_bytes();
+
+    while i < size {
+        let word_byte = first_word(&raw_bytes[i..size]);
+        let word = str::from_utf8(word_byte).unwrap();
+        words.push(word);
+        i = i + word.len() + 1;
+    }
+    return words;
+}
+
+fn first_word(s: &[u8]) -> &[u8] {
 
     let mut start = 0;
 
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use std::env;
-use std::str;
 mod finder;
 
 fn main() {
@@ -11,28 +10,12 @@ fn main() {
         return;
     }
 
-    let out = parse_words(&args[1]);
+    let out = finder::parse_words(&args[1]);
     output(out);
 
 }
 
-fn parse_words (s: &String) -> Vec<&str> {
 
-    let mut words: Vec<&str> = Vec::new();
-
-    //i will be a cursor that will hold the index to the first character of the next word
-    let mut i = 0;
-    let size = s.len();
-    let raw_bytes = s.as_bytes();
-
-    while i < size {
-        let word_byte = finder::first_word(&raw_bytes[i..size]);
-        let word = str::from_utf8(word_byte).unwrap();
-        words.push(word);
-        i = i + word.len() + 1;
-    }
-    return words;
-}
 
 fn output(words: Vec<&str>) {
     for (j, ref word) in words.iter().enumerate() {
